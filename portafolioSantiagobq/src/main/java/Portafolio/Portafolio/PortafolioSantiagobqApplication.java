30f75b0ed29fa9771846bdcef4fc7f87e0ba4dac
Santiago Brenes <itssanti_@outlook.com>
2025-02-03 20:13:35 -0600

    Add: Ejemplo inicial

---
+++ portafolioSantiagobq/src/main/java/Portafolio/Portafolio/PortafolioSantiagobqApplication.java
@@ -0,0 +1,13 @@
+package Portafolio.Portafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioSantiagobqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioSantiagobqApplication.class, args);
+	}
+
+}
