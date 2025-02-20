599a638286bff6aa4837d106d47ea29f6ab3030c
Santiago Brenes <itssanti_@outlook.com>
2025-02-19 18:19:14 -0600

    Actividad Springboot y Tymeleaf (Tab semana 4 U)

---
+++ ActividadSemana4/src/main/java/com/semanacuatro/actividadsemanacuatro/ActividadsemanacuatroApplication.java
@@ -0,0 +1,13 @@
+package com.semanacuatro.actividadsemanacuatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadsemanacuatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadsemanacuatroApplication.class, args);
+	}
+
+}
---
+++ ActividadSemana4/src/test/java/com/semanacuatro/actividadsemanacuatro/ActividadsemanacuatroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semanacuatro.actividadsemanacuatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadsemanacuatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
